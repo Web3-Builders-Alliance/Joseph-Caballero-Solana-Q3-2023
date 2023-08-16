469fec74b5d3c081c31afeb333befc7641d72c59
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-08-16 10:48:16 -0400

    spl_metadata portion complete

--- cluster1/ts/spl_metadata.ts
+++ cluster1/ts/spl_metadata.ts
@@ -1,5 +1,5 @@
 import { Commitment, Connection, Keypair, PublicKey, Transaction, sendAndConfirmTransaction } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import wallet from "../../wba-wallet.json";
 import { createCreateMetadataAccountV2Instruction, createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
 
 // We're going to import our keypair from the wallet file
@@ -10,7 +10,7 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Define our Mint address
-const mint = new PublicKey("<mint address>")
+const mint = new PublicKey("GnooAxSchwYt8Nrd557yTvJEv5urCuffHqrBMd398dbP")
 
 // Add the Token Metadata Program
 const token_metadata_program_id = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s')
@@ -23,9 +23,37 @@ const metadata_seeds = [
 ];
 const [metadata_pda, _bump] = PublicKey.findProgramAddressSync(metadata_seeds, token_metadata_program_id);
 
+
+
 (async () => {
     try {
         // Start here
+        const tx = new Transaction().add(createCreateMetadataAccountV3Instruction({
+          metadata: metadata_pda,
+          mint: mint,
+          mintAuthority: keypair.publicKey,
+          payer: keypair.publicKey,
+          updateAuthority: keypair.publicKey 
+        }, {
+          createMetadataAccountArgsV3: {
+            data: {
+              name: "wba-token",
+              symbol: "wba",
+              uri: "",
+              sellerFeeBasisPoints: 0,
+              creators: null,
+              collection: null,
+              uses: null,
+            },
+            isMutable: false,
+            collectionDetails: null,
+          }
+        }))
+        
+        const signature = await sendAndConfirmTransaction(connection, tx, [keypair]);
+
+        console.log(signature)
+
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
