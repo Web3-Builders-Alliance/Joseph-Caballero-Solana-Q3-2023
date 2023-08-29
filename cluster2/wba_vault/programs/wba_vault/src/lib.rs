01ecad5cfa5a7ad97d9305d94e16ba7561e3b624
Joseph-Caballero <joseph.w.caballero@gmail.com>
2023-08-29 19:33:41 -0400

    SPL deposit & withdraw

--- cluster2/wba_vault/programs/wba_vault/src/lib.rs
+++ cluster2/wba_vault/programs/wba_vault/src/lib.rs
@@ -1,10 +1,13 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program::{Transfer, transfer};
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{Transfer as SplTransfer, transfer as spl_transfer, TokenAccount, Token, Mint};
 
 declare_id!("EzdvFHcekYLwsqmZqADXwN6mv4BWea5aEdBojY5359rZ");
 
 #[program]
 pub mod wba_vault {
+
 
     use super::*;
 
@@ -51,6 +54,45 @@ pub mod wba_vault {
 
         transfer(cpi, amount)
     }
+
+    pub fn spl_deposit(ctx: Context<SPLDeposit>, amount: u64) -> Result<()> {
+        let accounts: SplTransfer<'_> = SplTransfer {
+            from: ctx.accounts.vault.to_account_info(),
+            to: ctx.accounts.owner_ata.to_account_info(),
+            authority: ctx.accounts.auth.to_account_info()
+        };
+
+        let cpi: CpiContext<'_, '_, '_, '_, _> = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            accounts
+        );
+
+        spl_transfer(cpi, amount)
+    }
+
+    pub fn spl_withdraw(ctx: Context<SPLDeposit>, amount: u64) -> Result<()> {
+        let accounts: SplTransfer<'_> = SplTransfer {
+            from: ctx.accounts.owner_ata.to_account_info(),
+            to: ctx.accounts.vault.to_account_info(),
+            authority: ctx.accounts.owner.to_account_info()
+        };
+
+        let seeds: &[&[u8]; 3] = &[
+            b"auth",
+            ctx.accounts.state.to_account_info().key.as_ref(),
+            &[ctx.accounts.state.auth_bump]
+        ];
+
+        let signer_seeds: &[&[&[u8]]; 1] = &[&seeds[..]];
+
+        let cpi: CpiContext<'_, '_, '_, '_, _> = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            accounts,
+            signer_seeds
+        );
+
+        spl_transfer(cpi, amount)
+    }
 }
 
 #[derive(Accounts)]
@@ -96,6 +138,79 @@ pub struct Payment<'info> {
     state: Account<'info, VaultState>,
     system_program: Program<'info, System>
 }
+
+#[derive(Accounts)]
+pub struct SPLDeposit<'info> {
+    #[account(mut)]
+    owner: Signer<'info>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = owner
+    )]
+    owner_ata: Account<'info, TokenAccount>,
+    mint: Account<'info, Mint>,
+    #[account(
+        seeds=[b"auth", state.key().as_ref()],
+        bump = state.auth_bump
+    )]
+    /// Check: This is safe
+    auth: UncheckedAccount<'info>,
+    #[account(
+        init,
+        payer = owner,
+        seeds = [b"spl_vault", state.key().as_ref()],
+        token::mint = mint,
+        token::authority = auth,
+        bump
+    )]
+    vault: Account<'info, TokenAccount>,
+    #[account(
+        seeds=[b"state", owner.key().as_ref()],
+        bump = state.state_bump
+    )]
+    state: Account<'info, VaultState>,
+    token_program: Program<'info, Token>,
+    associated_token_program: Program<'info, AssociatedToken>,
+    system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct SPLWithdraw<'info> {
+    #[account(mut)]
+    owner: Signer<'info>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = owner
+    )]
+    owner_ata: Account<'info, TokenAccount>,
+    mint: Account<'info, Mint>,
+    #[account(
+        seeds=[b"auth", state.key().as_ref()],
+        bump = state.auth_bump
+    )]
+    /// Check: This is safe
+    auth: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        close = owner,
+        seeds = [b"spl_vault", state.key().as_ref()],
+        token::mint = mint,
+        token::authority = auth,
+        bump
+    )]
+    vault: Account<'info, TokenAccount>,
+    #[account(
+        seeds=[b"state", owner.key().as_ref()],
+        bump = state.state_bump
+    )]
+    state: Account<'info, VaultState>,
+    token_program: Program<'info, Token>,
+    associated_token_program: Program<'info, AssociatedToken>,
+    system_program: Program<'info, System>
+}
+
 
 #[account]
 pub struct VaultState {
